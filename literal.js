fc2b636e418580e546653d7905a2748545499dc1
muhchiousadia <muhchiouchowdhurytunas@gmail.com>
2026-09-06 13:00:40 +0600

    Create literal.js

---
+++ literal.js
@@ -0,0 +1,19 @@
+const studentName = "Rahim";
+const studentAge = 22;
+
+const introduction = `My name is ${studentName} and I am ${studentAge} years old.`;
+
+console.log(introduction);
+
+const x = 15;
+const y = 25;
+
+console.log(`The total is ${x + y}`);
+
+const studentInfo = `
+Name: Rahim
+Age: 22
+Department: Computer Science
+`;
+
+console.log(studentInfo);
