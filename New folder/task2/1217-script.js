57f6c8d04892544ec17738b031a0c164ea48c941
muhchiousadia <muhchiouchowdhurytunas@gmail.com>
2026-09-19 11:58:53 -0700

    Add files via upload

---
+++ New folder/task2/1217-script.js
@@ -0,0 +1,42 @@
+
+fetch("1217-player.json")
+    .then(response => response.json())
+
+    .then(players => {
+
+        const container =
+            document.getElementById("player-container");
+
+        players.forEach(player => {
+
+            const card = document.createElement("div");
+
+            card.className = "card";
+
+            card.innerHTML = `
+                <h3>${player.name}</h3>
+
+                <p>
+                    Position: ${player.position}
+                </p>
+
+                <p>
+                    Number: ${player.number}
+                </p>
+
+                <p>
+                    Country: ${player.country}
+                </p>
+            `;
+
+            container.appendChild(card);
+
+        });
+
+    })
+
+    .catch(error => {
+
+        console.log("Error loading JSON:", error);
+
+    });
